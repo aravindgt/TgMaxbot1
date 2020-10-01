9fb4dca91f061a975da9e0130f8d65f9bbfe54a9
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-01 11:03:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer for File and Video Bot With Permanent Thumbnail support!💯</b> \n
 
 <b>Bot Maintained By: @JAsuran123</b> \n
 <b>Do /help for more...</b> \n
@@ -20,8 +20,8 @@ class Translation(object):
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /file fil name extension To rename a file</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /video fil name extension To rename a Streamable video File</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /renamev fil name extension To rename a Streamable video File</b>"
     REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
