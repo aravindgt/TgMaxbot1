33da06cc990b0eec2960312250dd0d0f523d22ca
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-18 18:27:47 +0530

    Update watermark.py

--- plugins/watermark.py
+++ plugins/watermark.py
@@ -29,7 +29,7 @@ from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from helper_funcs.display_progress import progress_for_pyrogram
 
 
-@pyrogram.on_message(Filters.private &  Filters.command("watermark"))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["watermark"]))
 async def _(c, m):
     
     if not await c.db.is_user_exist(m.chat.id):
